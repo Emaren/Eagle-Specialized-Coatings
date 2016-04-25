71b1be96152d542bedbba695f59af245685e35b5
Tony Blum <tonyblum@me.com>
2016-04-25 00:11:28 -0700

    added jquery titles to footer extended parallax to thermal,corrosion,fire

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,244 +18,367 @@
 //= require video
 //= require jquery.turbolinks
 
-  $(document).ready(function(){
+$(document).ready(function(){
 
 
 
-    $( ".magnifying-glass" ).mouseenter(function() {
-      $( ".search-box" ).fadeIn( "slow", function() {
-        $(this).animate()
-      });
+  $( ".magnifying-glass" ).mouseenter(function() {
+    $( ".search-box" ).fadeIn( "slow", function() {
+      $(this).animate()
     });
-    // Animation complete
-    $( ".magnifying-glass" ).mouseleave(function() {
-      $( ".search-box" ).fadeOut( "slow", function() {
-      });
-    });
-
-
-
-    $(".databaselayer").mouseenter(function() {
-      $(this).animate({
-        left: "1645px",
-        opacity: 1
-        }, {
-          duration: 450,
-          queue: false
-        });
-        $(".product-database-title").delay(230).show("slide", 250)
-      });
-
-    $(".databaselayer").mouseleave(function() {
-      $(this).animate({
-        left: "1800px",
-        opacity: 1
-        }, {
-          duration: 450,
-          queue: false
-        });
-        $(".product-database-title").fadeOut(200)
-      });
-
-
-
-      $(".corrosion-img-title").click(function(){
-        $(".thermal-img-title").fadeOut(1000)
-        $(".thermal-white").fadeOut(1000)
-        $(".thermal-box").fadeOut(1000)
-        $(".thermal-img").fadeOut(1000)
-        $(".thermal-vid").fadeOut(1000)
-        $(".play-thermal").fadeOut(1000)
-        $(".fire-img-title").fadeOut(1000)
-        $(".fire-white").fadeOut(1000)
-        $(".fire-box").fadeOut(1000)
-        $(".fire-img").fadeOut(1000)
-        $(".fire-vid").fadeOut(1000)
-        $(".play-fire").fadeOut(1000)
-        $(".corrosion-img-title").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".corrosion-white").animate({
-
-  }, 300 );
-        $(".corrosion-box").animate({
-
-  }, 300 );
-        $(".corrosion-img").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".corrosion-vid, .play-corrosion").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".play-corrosion").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-});
-
-
-      $(".thermal-img-title").click(function(){
-        $(".corrosion-img-title").fadeOut(1000)
-        $(".corrosion-white").fadeOut(1000)
-        $(".corrosion-box").fadeOut(1000)
-        $(".corrosion-img").fadeOut(1000)
-        $(".corrosion-vid").fadeOut(1000)
-        $(".play-corrosion").fadeOut(1000)
-        $(".fire-img-title").fadeOut(1000)
-        $(".fire-white").fadeOut(1000)
-        $(".fire-box").fadeOut(1000)
-        $(".fire-img").fadeOut(1000)
-        $(".fire-vid").fadeOut(1000)
-        $(".play-fire").fadeOut(1000)
-        $(".thermal-img-title").animate({
-          left: "+=515px"
-        }, 1000 );
-        $(".thermal-white").animate({
-          left: "+=515px"
-        }, 1000 );
-        $(".thermal-box").animate({
-          left: "+=515px"
-        }, 1000 );
-        $(".thermal-img").animate({
-          left: "+=515px"
-        }, 1000 );
-        $(".thermal-vid").animate({
-          left: "+=515px"
-        }, 1000 );
-        $(".play-thermal").animate({
-          left: "+=515px"
-        }, 1000 );
-      });
-
-
-      $(".fire-img-title").click(function(){
-        $(".thermal-img-title").fadeOut(1000)
-        $(".thermal-white").fadeOut(1000)
-        $(".thermal-box").fadeOut(1000)
-        $(".thermal-img").fadeOut(1000)
-        $(".thermal-vid").fadeOut(1000)
-        $(".play-thermal").fadeOut(1000)
-        $(".corrosion-img-title").fadeOut(1000)
-        $(".corrosion-white").fadeOut(1000)
-        $(".corrosion-box").fadeOut(1000)
-        $(".corrosion-img").fadeOut(1000)
-        $(".corrosion-vid").fadeOut(1000)
-        $(".play-corrosion").fadeOut(1000)
-        $(".fire-img-title").animate({
-          left: "-=515px"
-        }, 1000 );
-        $(".fire-white").animate({
-          left: "-=515px"
-        }, 1000 );
-        $(".fire-box").animate({
-          left: "-=515px"
-        }, 1000 );
-        $(".fire-img").animate({
-          left: "-=515px"
-        }, 1000 );
-        $(".fire-vid").animate({
-          left: "-=515px"
-        }, 2000 );
-        $(".play-fire").animate({
-          left: "-=515px"
-        }, 1000 );
-      });
-
-
-
-
-  $.fn.animateRotate = function(angle,prevAngle, duration, easing, complete) {
-      var args = $.speed(duration, easing, complete);
-      var step = args.step;
-      return this.each(function(i, e) {
-          args.complete = $.proxy(args.complete, e);
-          args.step = function(now) {
-              $.style(e, 'transform', 'rotate(' + now + 'deg)');
-              if (step) return step.apply(e, arguments);
-          };
-
-          $({deg: prevAngle}).animate({deg: angle}, args);
-      });
-  };
-  var angle = 0;
-  var prevAngle = 0;
-
-  $(".phone").mouseenter(function(e) {
-      prevAngle = angle
-      angle -= 100;
-      $(this).animateRotate(angle, prevAngle, 250);
-      e.preventDefault();
-      $(this).animate({
-        left: "800px",
-        opacity: 1
-      }, {
-        duration: 300,
-        queue: false
-      });
-      $(".phone-number").fadeIn(1000);
-      // $(".phone-number").show("slide", 400);
   });
-  $(".phone").mouseleave(function(e) {
-    prevAngle = angle
-    angle += 100;
-    $(this).animateRotate(angle, prevAngle, 350);
-    e.preventDefault();
+  // Animation complete
+  $( ".magnifying-glass" ).mouseleave(function() {
+    $( ".search-box" ).fadeOut( "slow", function() {
+    });
+  });
+
+
+
+  $(".databaselayer").mouseenter(function() {
     $(this).animate({
-      left: "905px",
+      left: "1645px",
       opacity: 1
       }, {
         duration: 450,
         queue: false
       });
-      $(".phone-number").hide("slide", "easeInQuart", 400);
+      $(".product-database-title").delay(230).show("slide", 250)
+    });
+
+  $(".databaselayer").mouseleave(function() {
+    $(this).animate({
+      left: "1800px",
+      opacity: 1
+      }, {
+        duration: 450,
+        queue: false
+      });
+      $(".product-database-title").fadeOut(200)
     });
 
 
 
-      $(".magnifying-glass").mouseenter(function(){
-        $(this).animate({
-          left: "1410px",
-          height: "+=180px",
-          width: "=+180px",
-        }, {
-          duration: 500,
-        });
+    $("#about-wrapper").hover(function() {
+      $(".about-us-title").show('clip', 400);
+    });
+      $("#about-wrapper").mouseleave(function() {
+        $(".about-us-title").hide('clip', 400);
       });
-      $(".magnifying-glass").mouseleave(function(){
-        $(this).animate({
-          left: "1800px",
-        }, {
-          duration: 500,
-        });
+    $("#contact-wrapper").mouseenter(function() {
+      $(".contact-us-title").show('clip', 400);
+    });
+      $("#contact-wrapper").mouseleave(function() {
+        $(".contact-us-title").hide('clip', 400);
+      });
+    $(".tw").mouseenter(function() {
+      $(".twitter-title").show('clip', 400);
+    });
+      $(".tw").mouseleave(function() {
+        $(".twitter-title").hide('clip', 400);
+      });
+    $("#facebook-wrapper").mouseenter(function() {
+      $(".facebook-title").show('clip', 400);
+    });
+      $("#facebook-wrapper").mouseleave(function() {
+        $(".facebook-title").hide('clip', 400);
+      });
+    $("#facebook-wrapper").mouseenter(function() {
+      $(".facebook-title").show('clip', 400);
+    });
+      $("#facebook-wrapper").mouseleave(function() {
+        $(".facebook-title").hide('clip', 400);
+      });
+    $("#youtube-wrapper").mouseenter(function() {
+      $(".youtube-title").show('clip', 400);
+    });
+      $("#youtube-wrapper").mouseleave(function() {
+        $(".youtube-title").hide('clip', 400);
+      });
+    $("#youtube-wrapper").mouseenter(function() {
+      $(".youtube-title").show('clip', 400);
+    });
+      $("#youtube-wrapper").mouseleave(function() {
+        $(".youtube-title").hide('clip', 400);
+      });
+    $("#industry-links-wrapper").mouseenter(function() {
+      $(".industry-links-title").show('clip', 400);
+    });
+      $("#industry-links-wrapper").mouseleave(function() {
+        $(".industry-links-title").hide('clip', 400);
+      });
+    $("#industry-links-wrapper").mouseenter(function() {
+      $(".industry-links-title").show('clip', 400);
+    });
+      $("#industry-links-wrapper").mouseleave(function() {
+        $(".industry-links-title").hide('clip', 400);
+      });
+    $("#certified-contractors-wrapper").mouseenter(function() {
+      $(".certified-contractors-title").show('clip', 400);
+    });
+      $("#certified-contractors-wrapper").mouseleave(function() {
+        $(".certified-contractors-title").hide('clip', 400);
+      });
+    $("#certified-contractors-wrapper").mouseenter(function() {
+      $(".certified-contractors-title").show('clip', 400);
+    });
+      $("#certified-contractors-wrapper").mouseleave(function() {
+        $(".certified-contractors-title").hide('clip', 400);
+      });
+    $("#pay-wrapper").mouseenter(function() {
+      $(".pay-title").show('clip', 400);
+    });
+      $("#pay-wrapper").mouseleave(function() {
+        $(".pay-title").hide('clip', 400);
+      });
+    $("#pay-wrapper").mouseenter(function() {
+      $(".pay-title").show('clip', 400);
+    });
+      $("#pay-wrapper").mouseleave(function() {
+        $(".pay-title").hide('clip', 400);
       });
 
 
+      $(".thermal-img-title").click(function(){
+        $(".corrosion-img-title").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".corrosion-white").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".corrosion-box").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".corrosion-img").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".corrosion-vid").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".play-corrosion").animate({
+          left: "-=515px",
+          opacity: .5,
+        }, 1500 );
+        $(".fire-img-title").animate({
+          opacity: .5,
+        }, 500 );
+        $(".fire-white").animate({
+          opacity: .5,
+        }, 500 );
+        $(".fire-box").animate({
+          opacity: .5,
+        }, 500 );
+        $(".fire-img").animate({
+          opacity: .5,
+        }, 500 );
+        $(".fire-vid").animate({
+          opacity: .1,
+        }, 500 );
+        $(".play-fire").animate({
+          opacity: .5,
+        }, 500 );
+        $(".thermal-img-title").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".thermal-white").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".thermal-box").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".thermal-img").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".thermal-vid").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".play-thermal").animate({
+          left: "+=515px"
+        }, 1500 );
+        $(".intro").hide('clip', 2000);
+        $(".footer-container").hide('clip', 2000);
+        $(".page-opening").show('scale', 1900);
+        $(".page-opening-bottom").show('scale', 1900);
+        $(".thermal-parallax").show('scale', 2500);
+      });
 
-      $(".thermal-img").hover(function(){
-        $(this).hide("blind");
-      })
 
-      $(".corrosion-img").mouseenter(function(){
-        $(this).hide("explode", 750);
-      })
+    $(".corrosion-img-title").click(function(){
+      $(".thermal-img-title").fadeOut(1000)
+      $(".thermal-white").fadeOut(1000)
+      $(".thermal-box").fadeOut(1000)
+      $(".thermal-img").fadeOut(1000)
+      $(".thermal-vid").fadeOut(1000)
+      $(".play-thermal").fadeOut(1000)
+      $(".fire-img-title").fadeOut(1000)
+      $(".fire-white").fadeOut(1000)
+      $(".fire-box").fadeOut(1000)
+      $(".fire-img").fadeOut(1000)
+      $(".fire-vid").fadeOut(1000)
+      $(".play-fire").fadeOut(1000)
+      $(".corrosion-img-title").animate({
+        height: "100%",
+        width: "100%",
+      }, 300 );
+            $(".corrosion-white").animate({
 
-      $(".fire-img").hover(function(){
-        $(this).hide("scale")
-      })
+      }, 300 );
+            $(".corrosion-box").animate({
 
-      $(".thermal-img").mouseleave(function(){
-        $(this).show("blind");
-      })
+      }, 300 );
+            $(".corrosion-img").animate({
+        height: "100%",
+        width: "100%",
+      }, 300 );
+            $(".corrosion-vid, .play-corrosion").animate({
+        height: "100%",
+        width: "100%",
+      }, 300 );
+            $(".play-corrosion").animate({
+        height: "100%",
+        width: "100%",
+      }, 300 );
+      $(".intro").hide('scale', 400);
+      $(".footer-container").hide('scale', 100);
+      $(".thermal-parallax").show('scale', 2500);
+    });
 
-      $(".corrosion-img").mouseleave(function(){
-        $(this).show("explode", 2000);
-      })
 
-      $(".fire-img").mouseleave(function(){
-        $(this).show("scale")
-      })
+
+
+    $(".fire-img-title").click(function(){
+      $(".thermal-img-title").fadeOut(1000)
+      $(".thermal-white").fadeOut(1000)
+      $(".thermal-box").fadeOut(1000)
+      $(".thermal-img").fadeOut(1000)
+      $(".thermal-vid").fadeOut(1000)
+      $(".play-thermal").fadeOut(1000)
+      $(".corrosion-img-title").fadeOut(1000)
+      $(".corrosion-white").fadeOut(1000)
+      $(".corrosion-box").fadeOut(1000)
+      $(".corrosion-img").fadeOut(1000)
+      $(".corrosion-vid").fadeOut(1000)
+      $(".play-corrosion").fadeOut(1000)
+      $(".fire-img-title").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".fire-white").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".fire-box").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".fire-img").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".fire-vid").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".play-fire").animate({
+        left: "-=515px"
+      }, 1000 );
+      $(".intro").hide('scale', 400);
+      $(".footer-container").hide('scale', 500);
+      $(".thermal-parallax").show('scale', 1000);
+    });
+
+
+
+
+$.fn.animateRotate = function(angle,prevAngle, duration, easing, complete) {
+    var args = $.speed(duration, easing, complete);
+    var step = args.step;
+    return this.each(function(i, e) {
+        args.complete = $.proxy(args.complete, e);
+        args.step = function(now) {
+            $.style(e, 'transform', 'rotate(' + now + 'deg)');
+            if (step) return step.apply(e, arguments);
+        };
+
+        $({deg: prevAngle}).animate({deg: angle}, args);
+    });
+};
+var angle = 0;
+var prevAngle = 0;
+
+$(".phone").mouseenter(function(e) {
+    prevAngle = angle
+    angle -= 100;
+    $(this).animateRotate(angle, prevAngle, 250);
+    e.preventDefault();
+    $(this).animate({
+      left: "800px",
+      opacity: 1
+    }, {
+      duration: 300,
+      queue: false
+    });
+    $(".phone-number").fadeIn(1000);
+    // $(".phone-number").show("slide", 400);
+});
+$(".phone").mouseleave(function(e) {
+  prevAngle = angle
+  angle += 100;
+  $(this).animateRotate(angle, prevAngle, 350);
+  e.preventDefault();
+  $(this).animate({
+    left: "905px",
+    opacity: 1
+    }, {
+      duration: 300,
+      queue: false
+    });
+    $(".phone-number").hide("slide", "easeInQuart", 300);
+  });
+
+
+
+    $(".magnifying-glass").mouseenter(function(){
+      $(this).animate({
+        left: "1410px",
+        height: "+=180px",
+        width: "=+180px",
+      }, {
+        duration: 500,
+      });
+    });
+    $(".magnifying-glass").mouseleave(function(){
+      $(this).animate({
+        left: "1800px",
+      }, {
+        duration: 500,
+      });
+    });
+
+
+
+    $(".thermal-img").hover(function(){
+      $(this).hide("blind");
+    })
+
+    $(".corrosion-img").mouseenter(function(){
+      $(this).hide("explode", 750);
+    })
+
+    $(".fire-img").hover(function(){
+      $(this).hide("scale")
+    })
+
+    $(".thermal-img").mouseleave(function(){
+      $(this).show("blind");
+    })
+
+    $(".corrosion-img").mouseleave(function(){
+      $(this).show("explode", 2000);
+    })
+
+    $(".fire-img").mouseleave(function(){
+      $(this).show("scale")
+    })
 
 })
